fbad11d51afbe1278b95c3f9743dc1721205bef2
exmyth <iexmyth@gmail.com>
2016-01-17 01:44:03 +0800

    add log4j and fastjson

--- src/test/java/com/exmyth/attendance/service/AccountServiceTest.java
+++ src/test/java/com/exmyth/attendance/service/AccountServiceTest.java
@@ -1,22 +1,29 @@
 package com.exmyth.attendance.service;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.alibaba.fastjson.JSON;
 import com.exmyth.attendance.model.Account;
 
 @RunWith(SpringJUnit4ClassRunner.class) // extends SpringJUnit4ClassRunner
 @ContextConfiguration(locations = { "classpath:spring.xml", "classpath:spring-mybatis.xml" })
 public class AccountServiceTest {
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = Logger.getLogger(AccountServiceTest.class);
+
 	@Autowired
 	private AccountService accountService;
 
 	@Test
 	public void test1() {
 		Account account = accountService.getAccountById(1);
-		System.out.println(account.getNickname());
+		logger.info(JSON.toJSONStringWithDateFormat(account,"yyyy-MM-dd HH:mm:dd"));
 	}
 }
